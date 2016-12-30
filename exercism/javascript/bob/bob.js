6e38ac2a73b2a7770601c102aaa7248116858974
ManuCiao <mn.sabatino@gmail.com>
2016-12-30 15:07:04 +0100

    complete Bob.js

--- exercism/javascript/bob/bob.js
+++ exercism/javascript/bob/bob.js
@@ -6,9 +6,16 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    if (!input.match(/\S/)) {
+        return 'Fine. Be that way!';
+    }
+    if (!input.match(/[a-z]/)) {
+        return 'Whoa, chill out!';
+    }
+    if (!input.match(/\w[^?]$/g)) {
+        return 'Sure.';
+    }
+    return 'Whatever.';
 };
 
 module.exports = Bob;
